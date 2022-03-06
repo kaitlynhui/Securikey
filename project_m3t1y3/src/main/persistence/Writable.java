1165f407b307ba11d72a30db9c2401f7aa4f9f06
kaitlynhui <56230966+kaitlynhui@users.noreply.github.com>
2022-03-06 14:36:55 -0800

    Delete project_m3t1y3 directory

--- project_m3t1y3/src/main/persistence/Writable.java
+++
@@ -1,9 +0,0 @@
-package persistence;
-
-import org.json.JSONObject;
-
-// represents an interface with a method that converts some object to a JSON object
-public interface Writable {
-    // EFFECTS: returns this as JSON object
-    JSONObject toJson();
-}
